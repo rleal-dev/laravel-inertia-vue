ecdacc11844756f4ed2456a8d09a502b8f19d14c
Rogerio Leal <rogerioleal1@gmail.com>
2024-10-12 16:20:36 +0100

    feat: Ajustes alerta.

--- resources/js/Components/AlertSuccess.vue
+++ resources/js/Components/AlertSuccess.vue
@@ -16,8 +16,8 @@ onUnmounted(() => clearInterval(timer))
 
 <template>
     <div v-if="message && isVisible" class="bg-teal-100 dark:bg-teal-900 rounded text-teal-900 dark:text-teal-100 px-4 py-3 mb-3" role="alert">
-        <div class="flex">
-            <svg class="fill-current h-6 w-6 text-teal-500 dark:text-teal-400 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+        <div class="flex items-center">
+            <svg class="fill-current h-5 w-5 text-teal-500 dark:text-teal-400 mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                 <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/>
             </svg>
             <p>{{ message }}</p>
